7bb4e04fb824d4329b6802e16af9fbc1127cab81
Watermelon-1234 <75419834+Watermelon-1234@users.noreply.github.com>
2024-07-24 13:10:27 +0800

    Update template_c++14_below.cpp

--- template_c++14_below.cpp
+++ template_c++14_below.cpp
@@ -1,4 +1,3 @@
-/*for c++14 or lower*/
 #pragma GCC optimize("O3,unroll-loops")
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -11,9 +10,9 @@
 #define S second
 #define mp make_pair
 #define pb emplace_back
-#define rep(i,n) for(i=0;i<(n);++i)
-#define foo(i,a,b) for(i=(a);i<=(b);++i)
-#define oof(i,a,b) for(i=(a);i>=(b);--i)
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define foo(i,a,b) for(int i=(a);i<=(b);++i)
+#define oof(i,a,b) for(int i=(a);i>=(b);--i)
 #define all(x) begin(x),end(x)
 #define btw(a,b,c) ((a)<=(b)&&(b)<=(c))
 using namespace __gnu_pbds;
@@ -21,33 +20,47 @@ using namespace std;
 typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
-template <class T>
-void debug_helper(int cnt, T arg) {
-    cerr << "\e[1;31m" << arg << "\e[0m\n";
+// Variadic debug function with variable name printing
+template <typename T>
+void print_debug(const char* name, T value) {
+    cerr << "\e[1;31m" << name << " = " << value;
 }
 
-template <class T, class ...Args>
-void debug_helper(int cnt, T arg, Args... args) {
-    cerr << "\e[1;31m" << arg << " ";
-    debug_helper(--cnt, args...);
+template <typename T, typename... Args>
+void print_debug(const char* names, T value, Args... args) {
+    const char* comma = strchr(names + 1, ',');
+    cerr.write(names, comma - names) << " = " << value;
+    if (comma) cerr << ", ";
+    print_debug(comma + 1, args...);
 }
 
-template <class ...Args>
-void debug(Args... args) {
-	cerr << "\e[1;31m" << "debug: ";
-    int cnt = sizeof...(Args);
-    debug_helper(cnt, args...);
+template <typename T, typename... Args>
+void formatter(const char* names, T value, Args... args) {
+    cerr << "\e[1;31m"<< "debug: [" ;
+    print_debug(names,value, args...);
+    cerr <<  "]"  << "\e[0m";
 }
-template <class T> void orange(T L, T R)
-{
-    cerr << "\e[1;31m"<<"orange:";
+
+#define debug(...) formatter(#__VA_ARGS__, __VA_ARGS__)
+
+// Orange function for range printing
+template <typename T>
+void orange(T L, T R) {
+    cerr << "\e[1;31morange: ";
     for (int f = 0; L != R; ++L) cerr << (f++ ? " " : "") << *L;
     cerr << "\e[0m\n";
 }
 
-#undef int 
-int main()
-{
+#undef int
+int main() {
     #define int int64_t
     fastio;
+
+    vector<int> v = {1, 2, 3, 4, 5};
+    orange(v.begin(), v.end());
+
+    int a = 10, b = 20, c = 30;
+    debug(a, b, c);
+    
+    return 0;
 }
